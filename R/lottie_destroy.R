ed1325105db2e1bcdc58e8aae908eb2754f21df2
CamHowitt <camhowitt96@gmail.com>
2024-06-05 17:44:40 +0100

    Implementation and documentation

---
+++ R/lottie_destroy.R
@@ -0,0 +1,37 @@
+#' @title Destroy a Lottie Animation
+#'
+#' @description Permanently destroy a specific Lottie animation or all Lottie animations.
+#'
+#' @param name A character string specifying the name of the Lottie animation to destroy.
+#' The default of "\code{all}" will destroy all animations within the Shiny application.
+#' @param session The Shiny session object. Defaults to the current reactive domain.
+#'
+#' @details Sends a custom session message \code{"lottie_js_destroy"} containing the function arguments.
+#'
+#' @seealso \code{\link{lottie_animation_methods}} for similar methods.
+#'
+#' @examplesIf interactive()
+#' library(shiny)
+#' library(shinyLottie)
+#'
+#' ui <- fluidPage(
+#'   include_lottie(),
+#'   lottie_animation(
+#'     path = "shinyLottie/example.json",
+#'     name = "my_animation"
+#'   ),
+#'   actionButton("destroy", "Destroy Animation")
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   observeEvent(input$destroy, {
+#'     lottie_destroy("my_animation")
+#'   })
+#' }
+#'
+#' shinyApp(ui, server)
+#'
+#' @export
+lottie_destroy <- function(name = "all", session = shiny::getDefaultReactiveDomain()) {
+  session$sendCustomMessage("lottie_js_destroy", list(name = name))
+}
---
+++ R/lottie_setSpeed.R
@@ -0,0 +1,42 @@
+#' @title Adjust Lottie Animation Speed
+#'
+#' @description Adjust the speed of an existing Lottie animation.
+#'
+#' @param speed A numeric specifying the desired animation speed.
+#' @param name A character string specifying the name of the Lottie animation to control.
+#' The default of "\code{all}" will control all animations within the Shiny application.
+#' @param session The Shiny session object. Defaults to the current reactive domain.
+#'
+#' @details Sends a custom session message \code{"lottie_js_setSpeed"} containing the function arguments.
+#'
+#' @note A speed of 1 will apply the default animation speed. Use a value between 0 and 1 for a slower animation speed.
+#' Applying a negative speed will also reverse the playback direction.
+#'
+#' @seealso \code{\link{lottie_animation_methods}} for similar methods.
+#'
+#' @examplesIf interactive()
+#' library(shiny)
+#' library(shinyLottie)
+#'
+#' ui <- fluidPage(
+#'   include_lottie(),
+#'   lottie_animation(
+#'     path = "shinyLottie/example.json",
+#'     name = "my_animation"
+#'   ),
+#'   numericInput("speed", "Speed", value = 1),
+#'   actionButton("updateSpeed", "Update Speed")
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   observeEvent(input$updateSpeed, {
+#'     lottie_setSpeed(speed = input$speed, name = "my_animation")
+#'   })
+#' }
+#'
+#' shinyApp(ui, server)
+#'
+#' @export
+lottie_setSpeed <- function(speed = 1, name = "all", session = shiny::getDefaultReactiveDomain()) {
+  session$sendCustomMessage("lottie_js_setSpeed", list(speed = speed, name = name))
+}
